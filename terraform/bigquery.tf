97d8c90797a49f6f8c3099bff49c92aa36e1eac7
arnaud.ly-ext <arnaud.ly-ext@kering.com>
2026-05-31 15:00:05 +0200

    feat(terraform): create external table from dbt sources file

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -1,29 +1,46 @@
 locals {
-  datasets = {
-    public = {
-      description = "Public dataset for open data and shared resources."
-    }
+  dbt_source = yamldecode(file("${path.module}/../dbt/models/sources.yml"))
 
-    private = {
-      description = "Private dataset for internal data."
+  ecommerce_source = one([
+    for s in local.dbt_source.sources : s
+    if s.name == local.config.bigquery.dbt_seeds.dbt_source_name
+  ])
 
-      # TODO set access
-      # access = [
-      #   {
-      #     role          = "OWNER"
-      #     user_by_email = google_service_account.bqowner.email
-      #   }
-      # ]
+  table_location_pairs = {
+    for t in local.ecommerce_source.tables :
+    t.name => {
+      location = t.external.location
     }
   }
 }
 
 
 resource "google_bigquery_dataset" "dataset" {
-  for_each = local.datasets
+  for_each = local.config.bigquery.datasets
 
   dataset_id                  = each.key
   description                 = each.value.description
-  location                    = "EU"
-  default_table_expiration_ms = 3600000 # 1 hour
+  location                    = each.value.location
+  default_table_expiration_ms = each.value.default_table_expiration_ms # 1 hour
+}
+
+# terraform/main.tf
+resource "google_bigquery_table" "landing_external_table" {
+  for_each = local.table_location_pairs
+
+  dataset_id = "landing"
+  table_id   = each.key
+
+  external_data_configuration {
+    source_uris = [each.value.location]
+    source_format = "CSV"
+
+    csv_options {
+      skip_leading_rows = 1
+      field_delimiter   = ","
+      quote             = "\""
+    }
+
+    autodetect = true
+  }
 }
